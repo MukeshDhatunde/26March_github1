58e992756ff45ef2ff907f7cd3a2fa3ea129ec0d
USER <USER@DESKTOP-1LGO7O0>
2022-07-31 19:48:11 +0530

    <Dear please find the attachment and correction required in TC-UN:101>

---
+++ src/sample1/demo.java
@@ -0,0 +1,15 @@
+package sample1;
+
+public class demo 
+{
+
+public static void main(String[] args) 
+{
+	System.out.println("Hello this is velocity");
+	System.out.println("UN-MukeshDhatunde1990");
+	System.out.println("PWd-MukeshDhatunde");
+	System.out.println("URL-https://github.com/MukeshDhatunde/26March_github.git");
+	System.out.println("Token-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");
+}
+}
+
